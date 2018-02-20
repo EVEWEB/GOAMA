140e8f60bdb2e5358d30262aba01ce9468e4d624
EVEWEB <esme.valentine@gmail.com>
2018-02-20 15:15:43 +0000

    mega waves

--- finectrl.glsl
+++ finectrl.glsl
@@ -8,8 +8,15 @@ uniform float time;
 
 float value (vec2 st) {
   vec2 m=u_mouse.xy;///u_resolution.xy;
-  return dot(st.xy, vec2((m.x*2.0)-1.0,(m.y*2.0)-1.0)*100.0); //vec2(0.1,0.99,0.1,0.99));
+  return dot(st.xy,
+    vec2((m.x*2.0)-1.0,(m.y*2.0)-1.0)*100.0); //vec2(0.1,0.99,0.1,0.99));
 }
+
+float oril (vec2 st) {
+    return sin(dot(st.xy,
+                         vec2(0.5,0.5))*u_time);
+}
+
 
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
@@ -17,7 +24,7 @@ void main() {
     st *= 20.0; // Scale the coordinate system by 10
     vec2 ipos = floor(st);  // get the integer coords
     vec2 fpos = fract(st);  // get the fractional coords
-    vec3 color = vec3(value( u_mouse.xy ));
+    vec3 color = vec3(oril( ipos ));
 //    vec3 color = vec3(fpos,1.0);
 
     gl_FragColor = vec4(color,1.0);
